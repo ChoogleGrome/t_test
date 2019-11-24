bd516b93ee3271a23f6045265b6ceea3c994fc4c
nesnah1 <u.nesnah@gmail.com>
2019-11-25 06:01:09 +1030

    Added Poseidon Tier, Added Ore

---
+++ Items/Poseidon_Tier/Ores/AquaticOre.cs
@@ -0,0 +1,20 @@
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace t_test.Items
+{
+	public class AquaticOre : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("Yea, it's wet");
+		}
+
+		public override void SetDefaults() {
+			item.width = 20;
+			item.height = 20;
+			item.maxStack = 999;
+			item.value = 100;
+			item.rare = 2;
+		}
